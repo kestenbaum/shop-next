9079687ac20f099bb1ddf5083e3a6317ee2c5115
vovne <kespa71for@gmail.com>
2025-06-19 11:59:07 +0200

    update api: [hook/useFetchResource] => fix bugs for get results

--- src/lib/api/hooks/useApiQuery.ts
+++ src/lib/api/hooks/useApiQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { api } from '@/lib/api/instance';
-import { AxiosResponse } from '@/lib/api/types';
+import {AxiosResponse} from '@/lib/api/types';
 
 export const useApiQuery = <T>(url: string, key: string) => {
   const { data, status, error, isLoading, isError } = useQuery<AxiosResponse<T>, AxiosError>({
