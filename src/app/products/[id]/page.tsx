b375f102ce9a63e7e0bf651d8be45ad08cf0c817
vovne <kespa71for@gmail.com>
2025-06-20 09:34:49 +0200

    update product/page: fix error

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
-import { getProductById } from '@/lib/products';
-
-type Props = {
-  params: { id: string; name: string };
-};
-
-export default async function Page({ params }: Props) {
-  if (!params.id) {
-    return <div> Product not find </div>;
-  }
-  const [product] = await Promise.all([getProductById(params.id)]);
-
+export default async function Page() {
   return (
     <div className="pt-15">
-      <h1>{product?.name}</h1>
+      <h1></h1>
     </div>
   );
 }
